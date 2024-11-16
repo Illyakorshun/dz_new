5393cb01543231fb6cd80e1bdc6321f1b562ca7e
vasya000 <vasya@gmail.com>
2024-11-16 17:20:42 +0200

    1

--- main.cpp
+++ main.cpp
@@ -2,19 +2,28 @@
 using namespace std;
 
 int main() {
-    int number, number2;
-    cout << "Enter a first number: ";
+    int number;
+    cout << "Enter a number: ";
     cin >> number;
 
-    cout << "Enter a second number: ";
-    cin >> number2;
-
-    if (number == number2) {
-        cout << "The number is positive" << endl;
+    if (number < 100000 || number > 999999) {
+        cout << "Error" << endl;
     }
     else {
-        cout << "The number is negativ" << endl;
+        int first = number / 1000;
+        int second = number % 1000;
+
+        int sumF = (first / 100) + ((first / 10) % 10) + (first % 10);
+        int sumS = (second / 100) + ((second / 10) % 10) + (second % 10);
+
+        if (sumF == sumS) {
+            cout << "The number is lucky" << endl;
+        }
+        else {
+            cout << "The number is not lucky" << endl;
+        }
     }
 
     return 0;
 }
+
