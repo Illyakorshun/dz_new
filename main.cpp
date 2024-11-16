8d8878185a0af511afe0e330a51933b6636db10f
vasya000 <vasya@gmail.com>
2024-11-16 17:29:29 +0200

    1234

--- main.cpp
+++ main.cpp
@@ -2,26 +2,25 @@
 using namespace std;
 
 int main() {
-    int number;
-    cout << "Enter  number: ";
-    cin >> number;
+    int numbers[7];
+    cout << "Enter:  " << endl;
 
-    if (number < 1000 || number > 9999) {
-        cout << "Error" << endl;
+    for (int i = 0; i < 7; ++i) {
+        cin >> numbers[i];
     }
-    else {
-        int d1 = (number / 1000) % 10;
-        int d2 = (number / 100) % 10;
-        int d3 = (number / 10) % 10;
-        int d4 = number % 10;
 
-        int swap = d2 * 1000 + d1 * 100 + d4 * 10 + d3;
-
-        cout << "The swapped number is: " << swap << endl;
+    int max = numbers[0];
+    for (int i = 1; i < 7; ++i) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
     }
+
+    cout << "The maximum number: " << max << endl;
 
     return 0;
 }
+
 
 
 
